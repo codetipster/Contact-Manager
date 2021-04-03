fb590f97486ec07313cbe1a647bb49ae5a888ce7
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2021-04-03 23:58:12 +0000

    configured static paths in settings.py file

--- contactapp/contactapp/settings.py
+++ contactapp/contactapp/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'contactapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
